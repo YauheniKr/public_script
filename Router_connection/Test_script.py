0e8c4f3ad19bf229790c097bb684eb5e4ad2adc5
YauheniKr <smilodon2@yandex.ru>
2017-12-26 16:07:48 +0300

    Update Test_script.py
    сделал подключение универсальным через telnet

--- Router_connection/Test_script.py
+++ Router_connection/Test_script.py
@@ -1,6 +1,8 @@
 import openpyxl
 import sys
 import getpass
+import telnetlib
+import time
 
 
 def open_excel_routers(file):
@@ -11,42 +13,78 @@ def open_excel_routers(file):
         addresses.append(sheet['A'+str(row)].value)
     return addresses
 
-def device_template(device_list, device_template):
-    device_temp = dict.fromkey(device_template)
-    for key in list(device_temp.keys()):
-        device
+def device_dict(device_list, device_template):
+    device_dict_list = []
+    for dev in device_list:
+        device_dic = dict.fromkeys(device_template)
+        for key in list(device_dic.keys()):
+            device_dic[key] = dev[(device_template.index(key))]
+            device_dict_list.append(device_dic)
+    return device_dict_list
 
 
+
+"""
 def send_show_command(device_list, command):
     key=[]
     result_comm_dict={}
     for dict in device_list:
         key = list(dict.keys())
         print('Подключаемся к устройству с IP адресом {} '.format(dict[key[1]]))
-        with ConnectHandler(**dict) as ssh:
-            ssh.enable()
-            result_comm = ssh.send_command(command)
+        with ConnectHandler(**dict) as telnet:
+            telnet.enable()
+            result_comm = telnet.send_command(command)
             print(result_comm)
             result_comm_dict[dict[key[1]]]=result_comm
     #print(result_comm_dict)
     return(result_comm_dict)
+"""
+
+
+def send_show_command(address_list, command, Username, Password):
+    output_list = []
+    for IP in address_list:
+        print('Connection to device {}'.format(IP))
+        with telnetlib.Telnet(IP) as t:
+            t.read_until(b'Username:')
+            t.write(Username + b'\n')
+
+            t.read_until(b'Password:')
+            t.write(Password + b'\n')
+            #t.write(b'enable\n')
+
+            #t.read_until(b'Password:')
+            #t.write(ENABLE_PASS + b'\n')
+            t.write(b'terminal length 0\n')
+            t.write(command + b'\n')
+
+            time.sleep(5)
+
+            output = t.read_very_eager().decode('utf-8')
+            output_list.append(output.strip())
+    return output_list
 
 device_list = []
 device = []
 device_template = ['device_type', 'ip', 'username', 'password', 'enable']
+command = 'show mac table'.encode('utf-8')
 
 print('Insert Username, Password and device type for connection')
-Username = input('Username:')
-Password = getpass.getpass()
+Username = input('Username:').encode('utf-8')
+Password = getpass.getpass().encode('utf-8')
 enable_Password = Password
-device_type = input('Device Type(cisco_ios for ssh or cisco_ios_telnet for telnet ):')
-device.append(device_type)
+#device_type = input('Device Type(cisco_ios for ssh or cisco_ios_telnet for telnet ):')
+#device.append(device_type)
 
 file = sys.argv[1]
-test = open_excel_routers(file)
-for address in test:
+address_list = open_excel_routers(file)
+for address in address_list:
     device.append(address)
     device = device + Username.split() + Password.split() + enable_Password.split()
     device_list.append(device)
-    device = []
-print(device_list)
+    #device = []
+device_list = device_dict(device_list, device_template)
+#command_listing = send_show_command(device_list, command)
+test = send_show_command(address_list, command, Username, Password)
+print(test)
+
