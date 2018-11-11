e21ff00847e87d7a8d246464b06a99507ea8b588
YauheniKr <smilodon2@yandex.ru>
2018-11-12 01:33:33 +0300

    Вынес вызов функций в отдельный файл

---
+++ router_connection/ZTE/connection.py
@@ -0,0 +1,20 @@
+from conn_api import init_logging, threads_conn, connect_ssh
+from conn_app import open_excel_file, router_params, take_command_line, input_credentials
+from conn_app import parser_show_clitable
+from tabulate import tabulate
+
+file = 'routers_1.xlsx'
+
+def main():
+    command = take_command_line()
+    init_logging('movie-app.log')
+    routers_parameter = open_excel_file(file)
+    router = router_params(routers_parameter)
+    inf = threads_conn(connect_ssh, router, command)
+    all_done = parser_show_clitable(''.join(list(inf.values())), command)
+    print(f'')
+    print(tabulate(all_done, headers = all_done[0], tablefmt = 'grid', stralign='center'))
+
+
+if __name__ == '__main__':
+    main()
