8638a2d1cf4beb57b2a032cd2fe72fe3989a0a18
yauheni <smilodon2@yandex.ru>
2018-11-28 18:56:34 +0300

    Merge branch 'master' of https://github.com/YauheniKr/public_script
    # Conflicts:
    #	router_connection/ZTE/routers.xlsx
    #	router_connection/ZTE/test_connection.py

--- router_connection/ZTE/connection.py
+++ router_connection/ZTE/connection.py
@@ -3,7 +3,7 @@ from conn_app import open_excel_file, router_params, take_command_line, input_cr
 from conn_app import parser_show_clitable
 from tabulate import tabulate
 
-file = 'routers_1.xlsx'
+file = './Routers/routers_test.xlsx'
 
 def main():
     init_logging('movie-app.log')
@@ -11,11 +11,12 @@ def main():
     router = router_params(routers_parameter)
     inf = threads_conn(connect_ssh, router)
     print(inf)
+    '''
     all_done = parser_show_clitable(inf)
     for output in all_done:
         print(f'')
         print(tabulate(output[1:], headers = output[0], tablefmt = 'grid', stralign='center'))
 
-
+'''
 if __name__ == '__main__':
     main()
